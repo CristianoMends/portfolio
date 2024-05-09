69ca24541c4ffa98d5ed9a4b98ccd6eece50947c
CristianoMends <mendescristiano012@gmail.com>
2024-05-09 14:36:06 -0300

    updated for mobile devices

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
+  @Input() message:string = '';
 
+  sendEmail(message: string): void {
+    const subject = encodeURIComponent('Interesse no Portfólio');
+    const emailBody = encodeURIComponent(message)
+    window.open(`mailto:mendescristiano012@gmail.com?subject=${subject}&body=${emailBody}`, '_blank')
+  }
 }
