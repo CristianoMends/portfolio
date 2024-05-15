cb1a219a25ba224383cf0d517b6a42ae01461999
CristianoMends <mendescristiano012@gmail.com>
2024-05-15 11:49:35 -0300

    updated design

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { ActiveSectionService } from '../section.service';
 
 @Component({
@@ -8,10 +8,32 @@ import { ActiveSectionService } from '../section.service';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent{
+export class HomeComponent implements AfterViewInit {
   constructor(private activeSectionService: ActiveSectionService) {}
  
   isCurrentActiveSection(): boolean {
     return this.activeSectionService.getActiveSection() === 'home';
+  }
+  @ViewChild('typingText') typingText!: ElementRef;
+
+  ngAfterViewInit(): void {
+    this.typeWriterEffect();    
+  }
+
+  typeWriterEffect() {
+    const element = this.typingText.nativeElement;
+    const text = element.textContent;
+    element.textContent = '';
+    let index = 0;
+
+    const type = () => {
+      if (index < text.length) {
+        element.textContent += text.charAt(index);
+        index++;
+        setTimeout(type, 100);
+      }
+    };
+
+    type();
   }
 }
