fa9fcde8c10bc33a76b19d8a45f766431c02d595
CristianoMends <mendescristiano012@gmail.com>
2024-06-11 13:52:20 -0300

    feat(update:home): update homepage design

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,35 +10,48 @@ import { CommonModule } from '@angular/common';
   styleUrl: './home.component.css'
 })
 export class HomeComponent implements AfterViewInit {
-  constructor(private activeSectionService: ActiveSectionService) {}
- 
+  constructor(private activeSectionService: ActiveSectionService) { }
+
   isCurrentActiveSection(): boolean {
     return this.activeSectionService.getActiveSection() === 'home';
   }
   @ViewChild('typingText') typingText!: ElementRef;
+  @ViewChild('name') name!: ElementRef;
   @ViewChild('subText') subText!: ElementRef;
   @ViewChild('arrow') arrow!: ElementRef;
   @ViewChild('video') video!: ElementRef;
+  @ViewChild('cv') cv!: ElementRef;
+  @ViewChild('contact') contact!: ElementRef;
 
   ngAfterViewInit(): void {
-    this.typeWriterEffect();    
+    const elements: ElementRef[] = [this.typingText, this.name, this.subText];
+    this.typeWriterEffect(elements);
   }
 
-  async typeWriterEffect() {
-    const element = this.typingText.nativeElement;
-    const text = element.textContent;
-    element.textContent = '';
+  async typeWriterEffect(textElements: ElementRef[]) {
+    for (let i = 0; i < textElements.length; i++) {
+      const element = textElements[i].nativeElement;
+      const text = element.textContent;
+      element.textContent = '';
+      element.className = 'typing-text visible'
 
-    for(let char in text){
-      
+      for (let char in text) {
+
         element.textContent += text.charAt(char);
-        if(element.textContent == text){
-          this.subText.nativeElement.className = 'show';    
-          this.arrow.nativeElement.className = 'scroll-down';
-          this.video.nativeElement.className = 'video-container';
+        if (element.textContent == text) {
+          element.className = '';
+
+          if(i == textElements.length - 1){
+            element.className = 'typing-text visible'
+            this.arrow.nativeElement.className = 'scroll-down';
+            this.video.nativeElement.className = 'video-container';
+            this.cv.nativeElement.className = 'cv visible';
+            this.contact.nativeElement.className = 'contact visible'
+          }              
         }
 
-      await this.sleep(100);
+        await this.sleep(50);
+      }
     }
   }
   sleep(ms: number) {
