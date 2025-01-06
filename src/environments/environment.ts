0b54253a650c9476e07ba2d1d78a06771ae3ef2b
CristianoMends <mendescristiano012@gmail.com>
2025-01-06 12:35:13 -0300

    add env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 import Environment from "./env";
 
 export const environment: Environment = {
-    apiUrl:'',
-    token:''
+    apiUrl: process.env['API_URL'] || '',
+    token: process.env['APP_TOKEN'] || ''
 };
